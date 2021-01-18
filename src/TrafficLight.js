54c66460e844772328208c483bc2dd11c38a6ca9
marisleidis84 <marisleidis84@gmail.com>
2021-01-17 21:49:35 -0300

    semaforo funcionando

--- src/TrafficLight.js
+++ src/TrafficLight.js
@@ -2,30 +2,35 @@ import React, { useState } from 'react'
 import './TrafficLight.css';
 
 const TrafficLight = () => {
-    let [ color, setColor] = useState("");
+    let [color, setColor] = useState("");
 
     let red_selected = "";
-            if (color === 'red') red_selected = 'selected';
+    if (color === 'red') red_selected = 'selected';
     let yellow_selected = "";
-        if (color === 'yellow') yellow_selected = 'selected';
+    if (color === 'yellow') yellow_selected = 'selected';
     let green_selected = "";
-        if (color === 'green') green_selected = 'selected';
+    if (color === 'green') green_selected = 'selected';
 
 
     return (
-
         <>
-            <div className='row1'>
+            <div className='row'>
                 <div className='col-md-12'>
                     <div className='col1 bg-dark'></div>
                 </div>
             </div>
-            <div className='row2'>
+            <div className='row'>
                 <div className='col-md-12'>
                     <div className='col2 bg-dark'>
-                        <input className={'input rounded-circle bg-danger' + red_selected} type='button' onClick={()=>setColor(color='red')}  />
-                        <input className={'input rounded-circle bg-warning' + yellow_selected} type='button' onClick={()=>setColor(color = 'yellow')}  />
-                        <input className={'input rounded-circle bg-success' + green_selected} type='button' onClick={()=>setColor(color = 'green')} />
+                        <div className={red_selected}>
+                            <input className='input rounded-circle bg-danger' type='button' onClick={() => setColor(color = 'red')} />
+                        </div>
+                        <div className={yellow_selected}>
+                            <input className='input rounded-circle bg-warning' type='button' onClick={() => setColor(color = 'yellow')} />
+                        </div>
+                        <div className={green_selected}>
+                            <input className='input rounded-circle bg-success' type='button' onClick={() => setColor(color = 'green')} />
+                        </div>
                     </div>
                 </div>
             </div>
